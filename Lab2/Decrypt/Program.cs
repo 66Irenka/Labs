cc1c80e3ffc6df919390ba8e2d732902e58cf399
66Irenka <pavlenkoiren66@gmail.com>
2026-04-09 15:12:20 +0300

    Add Lab2 Caesar cipher projects

---
+++ Lab2/Decrypt/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string file2 = "Iryna2.txt";
+        string file3 = "Iryna3.txt";
+
+        if (File.Exists(file2))
+        {
+            Console.WriteLine($"Читання файлу {file2}...");
+
+            string encryptedText = File.ReadAllText(file2);
+            string decryptedText = DecryptCaesar(encryptedText);
+
+            File.WriteAllText(file3, decryptedText);
+
+            Console.WriteLine("Текст розшифровано!");
+            Console.WriteLine($"Результат у файлі: {file3}");
+            Console.WriteLine("Розшифрований текст:");
+            Console.WriteLine(decryptedText);
+        }
+        else
+        {
+            Console.WriteLine("Файл Iryna2.txt не знайдено.");
+        }
+    }
+
+    static string DecryptCaesar(string input)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        foreach (char c in input)
+        {
+            if (c == 'A') { sb.Append('Щ'); continue; }
+            if (c == 'B') { sb.Append('Ю'); continue; }
+            if (c == 'C') { sb.Append('Я'); continue; }
+            if (c == 'd') { sb.Append('ь'); continue; }
+
+            if (char.IsLetter(c))
+                sb.Append((char)(c - 3));
+            else
+                sb.Append(c);
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ Lab2/Encrypt/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string file1 = "Iryna1.txt";
+        string file2 = "Iryna2.txt";
+
+        if (!File.Exists(file1)) File.Create(file1).Close();
+        if (!File.Exists(file2)) File.Create(file2).Close();
+
+        Console.WriteLine($"Відкрийте файл {file1}, введіть текст і збережіть.");
+        Console.WriteLine("Після цього натисніть Enter...");
+        Console.ReadLine();
+
+        string originalText = File.ReadAllText(file1);
+        string encryptedText = EncryptCaesar(originalText);
+
+        File.WriteAllText(file2, encryptedText);
+
+        Console.WriteLine("Текст зашифровано!");
+        Console.WriteLine($"Результат у файлі: {file2}");
+        Console.WriteLine("Зашифрований текст:");
+        Console.WriteLine(encryptedText);
+    }
+
+    static string EncryptCaesar(string input)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        foreach (char c in input)
+        {
+            if (c == 'Щ') { sb.Append('A'); continue; }
+            if (c == 'Ю') { sb.Append('B'); continue; }
+            if (c == 'Я') { sb.Append('C'); continue; }
+            if (c == 'ь') { sb.Append('d'); continue; }
+
+            if (char.IsLetter(c))
+                sb.Append((char)(c + 3));
+            else
+                sb.Append(c);
+        }
+
+        return sb.ToString();
+    }
+}
